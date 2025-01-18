992b4a8ab2d6367363c334d3bca2d190218e3eb5
volard <kirillzhilenkov@gmail.com>
2025-01-18 16:59:49 +0000

    Edit dotfiles.sh

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -12,9 +12,9 @@ echo
 # Clone repository or pull
 if ! [[ -d ${DOTFILES_DIR} ]]; then
 	if ! [[ -d "${DOTFILES_DIR}/.git" ]]; then
-		git -C "${DOTFILES_DIR}" pull
-	else
 		git clone "https://gitlab.com/dotfiles30/comma_files.git" "${DOTFILES_DIR}"
+	else
+		git -C "${DOTFILES_DIR}" pull
 	fi
 else
 	git -C "${DOTFILES_DIR}" pull
