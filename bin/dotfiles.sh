44d7569c80d63177a4e7add12b4524e9032bd357
volard <kirillzhilenkov@gmail.com>
2025-01-24 01:13:04 +0800

    upgrade

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -8,12 +8,12 @@ PASSWORD_FILE_PATH="${DOTFILES_DIR}/vault-password.txt"
 
 # Clone repository or pull
 if ! [[ -d ${DOTFILES_DIR} ]]; then
-	git clone "https://gitlab.com/dotfiles30/comma_files.git" "${DOTFILES_DIR}"
+  git clone "https://gitlab.com/dotfiles30/comma_files.git" "${DOTFILES_DIR}"
 else
-        if ! [[ -d "${DOTFILES_DIR}/.git" ]]; then
-                git clone "https://gitlab.com/dotfiles30/comma_files.git" "${DOTFILES_DIR}"
-        else
-		git -C "${DOTFILES_DIR}" pull
+  if ! [[ -d "${DOTFILES_DIR}/.git" ]]; then
+    git clone "https://gitlab.com/dotfiles30/comma_files.git" "${DOTFILES_DIR}"
+  else
+    git -C "${DOTFILES_DIR}" pull
 	fi
 fi
 
@@ -23,37 +23,37 @@ chmod +x "${DOTFILES_DIR}/bin/dotfiles.sh"
 
 # Determine distro
 if [[ -f /etc/os-release ]]; then
-        # Modern distributions use os-release file
-        . /etc/os-release
-        distro=${ID}
-        version=${VERSION_ID}
+  # Modern distributions use os-release file
+  . /etc/os-release
+  distro=${ID}
+  version=${VERSION_ID}
 elif [[ -f /etc/lsb-release ]]; then
-        # Older Ubuntu/Debian systems
-        . /etc/lsb-release
-        distro=${DISTRIB_ID}
-        version=${DISTRIB_RELEASE}
+  # Older Ubuntu/Debian systems
+  . /etc/lsb-release
+  distro=${DISTRIB_ID}
+  version=${DISTRIB_RELEASE}
 elif [[ -f /etc/redhat-release ]]; then
-        # Older Red Hat based systems
-        distro=$(cat /etc/redhat-release)
+  # Older Red Hat based systems
+  distro=$(cat /etc/redhat-release)
 else
-        # Fallback
-        distro=$(uname -s)
-        version=$(uname -r)
+  # Fallback
+  distro=$(uname -s)
+  version=$(uname -r)
 fi
 
-echo "==== Configuring system ===="
+echo "\n==== Configuring system ====\n"
 echo "Distribution: ${distro}"
 echo "Version: ${version}"
-echo "==== ------------------ ===="
+echo "\n==== ------------------ ====\n"
 
 # Install Ansible
 if ! [[ -x "$(command -v ansible || true)" ]]; then
-        if [[ ${distro,,} == "arch" ]]; then
-                sudo pacman -S ansible --noconfirm
-        else
+    if [[ ${distro,,} == "arch" ]]; then
+      sudo pacman -S ansible --noconfirm
+    else
 		# Update the system initially
-                sudo apt-get update
-                sudo apt-get upgrade
+    sudo apt-get update
+    sudo apt-get upgrade
 
 		# pipx is the best method to install ansible imo
 		sudo apt install pipx -y
@@ -63,14 +63,14 @@ if ! [[ -x "$(command -v ansible || true)" ]]; then
 		source ~/.bashrc
 
 		if ! [[ ":$PATH:" == *":$DIR:"* ]]; then
-		    echo 'PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+      echo 'PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 		fi
 
 		source ~/.bashrc
 
 		# Ansible installation itself
 		pipx install --include-deps ansible
-        fi
+  fi
 fi
 
 
@@ -81,11 +81,11 @@ ansible-galaxy install -r requirements.yml
 # Setup password if not exists
 if ! [[ -f "${DOTFILES_DIR}/vault-password.txt" ]]; then
         read -pr "Please enter your Ansible Vault password: " ANSIBLE_PASSWORD
-        echo "${ANSIBLE_PASSWORD}" >>"${PASSWORD_FILE_PATH}"
+        echo "${ANSIBLE_PASSWORD}" > "${PASSWORD_FILE_PATH}"
 fi
 
 # Run playbook
-ansible-playbook --diff --vault-password-file "${PASSWORD_FILE_PATH}" "${DOTFILES_DIR}/main.yml" "$@" --ask-become-pass
+ansible-playbook --diff --vault-password-file "${PASSWORD_FILE_PATH}" "${DOTFILES_DIR}/main.yml" --ask-become-pass "$@"
 
 # vi:ft=sh:
 
