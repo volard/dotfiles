d3eda2977270ebb30deb463b4499f7076648d8f7
volard <kirillzhilenkov@gmail.com>
2025-02-17 04:05:39 +0000

    Edit endeavouros.sh

--- docs/endeavouros.sh
+++ docs/endeavouros.sh
@@ -5,12 +5,11 @@ sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
 
 
-sudo pacman -S docker
+sudo pacman -S docker docker-compose
 sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl enable docker
 sudo systemctl start docker
 yay -S lazydocker
 
-sudo pacman -S lazygit neovim lf
-sudo pacman -S neofetch eza bat git-delta
+sudo pacman -S lazygit neovim lf neofetch eza bat git-delta ansible
