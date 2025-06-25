f8a3e1ee5e6aa174bd06c106a6770683d51cfd33
volard <kirillzhilenkov@gmail.com>
2025-06-26 00:09:27 +0800

    add latex

--- roles/lazy_neovim/files/lua/plugins/latex.lua
+++ roles/lazy_neovim/files/lua/plugins/latex.lua
@@ -1,8 +1,17 @@
 return {
   "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
+  -- lazy-loading will disable inverse search
+  lazy = false, -- we don't want to lazy load VimTeX
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "zathura"
-  end
+
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = "./.latexmk/aux",
+      out_dir = "./.latexmk/out",
+    }
+  end,
+  keys = {
+    { "<localLeader>l", "", desc = "+vimtex" },
+  },
 }
