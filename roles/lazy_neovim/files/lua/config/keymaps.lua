09aafdfffe4d6646938934d3cf5c3cfbbe4c2054
volard <kirillzhilenkov@gmail.com>
2025-06-28 17:03:40 +0800

    aihsoetn

--- roles/lazy_neovim/files/lua/config/keymaps.lua
+++ roles/lazy_neovim/files/lua/config/keymaps.lua
@@ -13,4 +13,5 @@ map("v", "<C-p>", '"0p', { desc = 'Paste last copyed "0 regester content' })
 
 -- map("n", "<space>fb", "<cmd>Telescope file_browser<CR>", { desc = "Open file browser" })
 
+-- https://miguelcrespo.co/posts/using-telescope-to-find-text-inside-paths
 map("n", "<leader>fs", require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
