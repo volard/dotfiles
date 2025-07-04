b7e631fc8ba414af7dee61231ea9997f51482c85
volard <kirillzhilenkov@gmail.com>
2025-07-04 16:21:11 +0800

    enhance tmux + add dynamic wallpapers

--- roles/sway/files/dynamic_wallpapers.sh
+++ roles/sway/files/dynamic_wallpapers.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
 
 # Directory containing wallpapers
-WALLPAPER_DIR="$HOME/dotfiles/files/wallpapers"  # Change this to your wallpaper directory
+WALLPAPER_DIR="$HOME/dotfiles/files/wallpapers"
 LOCKFILE="/tmp/wallpaper_changer.lock"
 
+killall swaybg
 
-# Function to set a random wallpaper
 set_random_wallpaper() {
-    # Kill any existing swaybg process
-    pkill swaybg
 
-    # Get a random wallpaper from the specified directory
-    wallpaper=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-    
-    # Set the wallpaper using `sway` or `wlroot` (adjust according to your compositor)
-    # swaymsg output '*' bg "$wallpaper" fill
-    swaybg -i "$wallpaper" -m fill &
+	wallpaper=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+	swaybg -i "$wallpaper" -m fill &
+
+	PID=$(pidof swaybg)
+  sleep 600
+
+  kill $PID
 }
 
-# Check if another instance is running
-if [ -e "$LOCKFILE" ]; then
-    echo "Another instance is already running."
-    exit 1
-fi
+# # Check if another instance is running
+# if [ -e "$LOCKFILE" ]; then
+# 	echo "Another instance is already running."
+# 	exit 1
+# fi
+#
+# # Create a lock file
+# touch "$LOCKFILE"
+#
+# # Cleanup lock file on exit
+# trap 'rm -f "$LOCKFILE"; exit' INT TERM EXIT
 
-# Create a lock file
-touch "$LOCKFILE"
-
-# Cleanup lock file on exit
-trap 'rm -f "$LOCKFILE"; exit' INT TERM EXIT
-
-# Main loop to change wallpaper every 10 seconds
 while true; do
-    set_random_wallpaper
-    sleep 600
+	set_random_wallpaper
 done
-
